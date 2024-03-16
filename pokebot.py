51999f8929adc0a01bcc056855803ed22a724b13
sarveshash <136432405+sarveshash@users.noreply.github.com>
2024-03-16 10:19:03 +0530

    Update pokebot.py

--- pokebot.py
+++ pokebot.py
@@ -1,13 +1,10 @@
-import asyncio
+
 from pyrogram import Client
 
 api_id = 20058505
 api_hash = "c6416428be72db3174999c1740524b88"
+bot_token = "6563744619:AAG4v_ABfLA3lCSGbcNLWlS07ZA_qUmseqM"
 
+app = Client("my_account", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-async def main():
-    async with Client("my_account", api_id, api_hash) as app:
-        await app.send_message("me", "Greetings from **Pyrogram**!")
-
-
-asyncio.run(main())
+app.run()
