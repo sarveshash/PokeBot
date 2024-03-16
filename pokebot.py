142e7fcd8cb7397948e414b3e54fcfaf3b371e1e
sarveshash <136432405+sarveshash@users.noreply.github.com>
2024-03-16 10:21:18 +0530

    Update pokebot.py

--- pokebot.py
+++ pokebot.py
@@ -1,10 +1,8 @@
 
 from pyrogram import Client
 
-api_id = 20058505
-api_hash = "c6416428be72db3174999c1740524b88"
-bot_token = "6563744619:AAG4v_ABfLA3lCSGbcNLWlS07ZA_qUmseqM"
 
-app = Client("my_account", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+app = Client("my_account")
 
 app.run()
