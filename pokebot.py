2054c1bf5ca4c8495c52802191e169ae7226390c
sarveshash <136432405+sarveshash@users.noreply.github.com>
2024-03-16 10:40:36 +0530

    Update pokebot.py

--- pokebot.py
+++ pokebot.py
@@ -1,7 +1,11 @@
 import asyncio
 from pyrogram import Client
 
-app = Client("Bot")
+api_id = 20058505
+api_hash = "c6416428be72db3174999c1740524b88"
+bot_token = "6563744619:AAG4v_ABfLA3lCSGbcNLWlS07ZA_qUmseqM"
+
+app = Client("Bot",api_id = api_id, api_hash = api_hash, bot_token = bot_token)
 
 async def main():
   async with app:
