2a3b78d7a71282b3ab117d7dc3061d6b962c1427
sarveshash <136432405+sarveshash@users.noreply.github.com>
2024-03-16 09:40:22 +0530

    Update pokebot.py

--- pokebot.py
+++ pokebot.py
@@ -1,10 +1,10 @@
-from pyrogram import client, filters
+from pyrogram import Client, filters
 
 api_id = 20058505
 api_hash = "c6416428be72db3174999c1740524b88"
 bot_token = "6563744619:AAG4v_ABfLA3lCSGbcNLWlS07ZA_qUmseqM"
 
-app = client("Join Bot",api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client("Join Bot",api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_mesaage(filters.command('start')&filters.private)
 async def start(client, message):
